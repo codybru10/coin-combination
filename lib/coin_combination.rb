a1307b5ff8bd64c3357daa014e0494e3c8465c22
Cody Brubaker and Ezra Sandzer-Bell <ezra@>
2016-11-17 10:19:45 -0800

    establish working method for coin counter with fixnum values of 1-24

--- lib/coin_combination.rb
+++ lib/coin_combination.rb
@@ -1,5 +1,64 @@
-class String
-  define_method(:method_template) do |variable|
+class Fixnum
+  define_method(:coin_combination) do
+    coins = {1 => "penny", 5 => "nickel", 10 => "dimes", 25 => "quarter"}
+    outputString =""
+    counter = 0
+    until counter.==(self)
+      counter = counter.+(1)
+    end
 
+    if counter == 1
+      pennyString = counter.to_s + " penny"
+      outputString = pennyString
+    elsif counter <=4 && counter > 1
+      pennyString = counter.to_s + " pennies"
+      outputString = pennyString
+    elsif counter ==5
+      nickelString = (counter/5).to_s + " nickel"
+      outputString = nickelString
+    elsif counter >=5 && counter <=9
+      nickelMod = (counter%5)
+      if nickelMod == 1
+        nickelString = (counter/5).floor.to_s + " nickel " + (counter%5).to_s + " penny"
+      elsif nickelMod > 1 and nickelMod < 5
+        nickelString = (counter/5).floor.to_s + " nickel " + (counter%5).to_s + " pennies"
+      end
+      outputString = nickelString
+    elsif counter ==10
+      dimeString = (counter/10).to_s + " dime"
+      outputString = dimeString
+    elsif counter >= 11 && counter <= 24
+      dimeMod = (counter%10)
+      dimeQuotient = (counter/10)
+      nickelQuotient = ((counter%10)/5)
+      pennyMod = ((counter%10)%5)
+      if counter == 11
+        dimeString = dimeQuotient.floor.to_s + " dime " + (counter%10).to_s + " penny"
+        outputString = dimeString
+      elsif counter >= 12 and counter <=14
+        dimeString = dimeQuotient.floor.to_s + " dime " + (counter%10).to_s + " pennies"
+        outputString = dimeString
+      elsif counter == 15
+        dimeString = dimeQuotient.floor.to_s + " dime " + nickelQuotient.to_s + " nickel"
+        outputString = dimeString
+      elsif counter == 16
+        dimeString = dimeQuotient.floor.to_s + " dime " + nickelQuotient.floor.to_s + " nickel " + pennyMod.to_s + " penny"
+        outputString = dimeString
+      elsif counter >= 17 and counter <= 19
+        dimeString = dimeQuotient.floor.to_s + " dime " + nickelQuotient.floor.to_s  + " nickel " + pennyMod.to_s + " pennies"
+        outputString = dimeString
+      elsif counter == 20
+        dimeString = (counter/10).to_s + " dimes"
+        outputString = dimeString
+      elsif counter == 21
+        dimeString = dimeQuotient.floor.to_s + " dimes " + pennyMod.to_s + " penny"
+        outputString = dimeString
+      elsif counter >= 22 and counter <=24
+        dimeString = dimeQuotient.floor.to_s + " dimes " + pennyMod.to_s + " pennies"
+        outputString = dimeString
+      end
+    end
+
+    outputString
   end
 end
--- spec/coin_combination_spec.rb
+++ spec/coin_combination_spec.rb
@@ -1,9 +1,43 @@
 require('rspec')
 require('pry')
-require('template.rb')
+require('coin_combination.rb')
 
-describe('ClassType#template') do
-  it("what it does") do
-    expect("input".beats("paramater")).to(eq("output"))
+describe('Fixnum#coin_combination') do
+  it("test Fixnum for single penny") do
+    expect(1.coin_combination()).to(eq("1 penny"))
   end
+  it("test Fixnum for quantity of pennies up to 4 cents") do
+    expect(4.coin_combination()).to(eq("4 pennies"))
+  end
+  it("test Fixnum for single nickel") do
+    expect(5.coin_combination()).to(eq("1 nickel"))
+  end
+  it("test Fixnum for modulo") do
+    expect(7.coin_combination()).to(eq("1 nickel 2 pennies"))
+  end
+  it("test Fixnum for single dime") do
+    expect(10.coin_combination()).to(eq("1 dime"))
+  end
+  it("test Fixnum for modulo") do
+    expect(12.coin_combination()).to(eq("1 dime 2 pennies"))
+  end
+  it("test Fixnum for modulo") do
+    expect(15.coin_combination()).to(eq("1 dime 1 nickel"))
+  end
+  it("test Fixnum for modulo") do
+    expect(16.coin_combination()).to(eq("1 dime 1 nickel 1 penny"))
+  end
+  it("test Fixnum for modulo") do
+    expect(17.coin_combination()).to(eq("1 dime 1 nickel 2 pennies"))
+  end
+  it("test Fixnum for modulo") do
+    expect(20.coin_combination()).to(eq("2 dimes"))
+  end
+  it("test Fixnum for modulo") do
+    expect(21.coin_combination()).to(eq("2 dimes 1 penny"))
+  end
+  it("test Fixnum for modulo") do
+    expect(23.coin_combination()).to(eq("2 dimes 3 pennies"))
+  end
+
 end
