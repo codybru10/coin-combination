fa65904dd1e610f8b306398d52c959923e41e0ce
Cody Brubaker and Ezra Sandzer-Bell <ezra@>
2016-11-17 11:16:35 -0800

    iterate fixnum through ninety-nine cents

--- lib/coin_combination.rb
+++ lib/coin_combination.rb
@@ -33,10 +33,10 @@ class Fixnum
       nickelQuotient = ((counter%10)/5)
       pennyMod = ((counter%10)%5)
       if counter == 11
-        dimeString = dimeQuotient.floor.to_s + " dime " + (counter%10).to_s + " penny"
+        dimeString = dimeQuotient.floor.to_s + " dime " + dimeMod.to_s + " penny"
         outputString = dimeString
       elsif counter >= 12 and counter <=14
-        dimeString = dimeQuotient.floor.to_s + " dime " + (counter%10).to_s + " pennies"
+        dimeString = dimeQuotient.floor.to_s + " dime " + dimeMod.to_s + " pennies"
         outputString = dimeString
       elsif counter == 15
         dimeString = dimeQuotient.floor.to_s + " dime " + nickelQuotient.to_s + " nickel"
@@ -57,8 +57,102 @@ class Fixnum
         dimeString = dimeQuotient.floor.to_s + " dimes " + pennyMod.to_s + " pennies"
         outputString = dimeString
       end
+    elsif counter >= 25 && counter <= 99
+      quarterQuotient = (counter/25)
+      quarterMod = (counter%25)
+      dimeQuotient = (quarterMod/10)
+      dimeMod = (counter%10)
+      nickelQuotient = (dimeMod/5)
+      pennyMod = (quarterMod%5)
+      if counter == 25
+        quarterString = (counter/25).to_s + " quarter"
+        outputString = quarterString
+      elsif counter >= 26 && counter <= 29
+        quarterString = quarterQuotient.floor.to_s + " quarter " + quarterMod.to_s + " penny"
+        outputString = quarterString
+      elsif counter == 30
+        quarterString = quarterQuotient.floor.to_s + " quarter " + nickelQuotient.to_s + " nickel"
+        outputString = quarterString
+      elsif counter == 31
+        quarterString = quarterQuotient.floor.to_s + " quarter " + nickelQuotient.floor.to_s + " nickel " + pennyMod.to_s + " penny"
+        outputString = quarterString
+      elsif counter >= 32 && counter <= 34
+        quarterString = quarterQuotient.floor.to_s + " quarter " + nickelQuotient.floor.to_s  + " nickel " + pennyMod.to_s + " pennies"
+        outputString = quarterString
+      elsif counter == 35
+      quarterString = quarterQuotient.floor.to_s + " quarter " + dimeQuotient.floor.to_s + " dime"
+        outputString = quarterString
+      elsif counter == 36
+      quarterString = quarterQuotient.floor.to_s + " quarter " + dimeQuotient.floor.to_s + " dime " + pennyMod.to_s + " penny"
+        outputString = quarterString
+      elsif counter >= 37 && counter <= 39
+      quarterString = quarterQuotient.floor.to_s + " quarter " + dimeQuotient.floor.to_s + " dime " + pennyMod.to_s + " pennies"
+        outputString = quarterString
+      elsif counter == 40
+      quarterString = quarterQuotient.floor.to_s + " quarter " + dimeQuotient.floor.to_s + " dime " + nickelQuotient.to_s + " nickel"
+        outputString = quarterString
+      elsif counter == 41
+      quarterString = quarterQuotient.floor.to_s + " quarter " + dimeQuotient.floor.to_s + " dime " + nickelQuotient.to_s + " nickel " + pennyMod.to_s + " penny"
+        outputString = quarterString
+      elsif counter >= 42 && counter <= 44
+      quarterString = quarterQuotient.floor.to_s + " quarter " + dimeQuotient.floor.to_s + " dime " + nickelQuotient.to_s + " nickel " + pennyMod.to_s + " pennies"
+        outputString = quarterString
+      elsif counter == 45
+      quarterString = quarterQuotient.floor.to_s + " quarter " + dimeQuotient.floor.to_s + " dimes"
+        outputString = quarterString
+      elsif counter == 46
+      quarterString = quarterQuotient.floor.to_s + " quarter " + dimeQuotient.floor.to_s + " dimes " +  pennyMod.to_s + " penny"
+        outputString = quarterString
+      elsif counter >= 47 && counter <= 49
+      quarterString = quarterQuotient.floor.to_s + " quarter " + dimeQuotient.floor.to_s + " dimes " + nickelQuotient.to_s + " nickel " + pennyMod.to_s + " pennies"
+        outputString = quarterString
+      elsif counter == 50 || counter == 75
+        quarterString = quarterQuotient.floor.to_s + " quarters"
+        outputString = quarterString
+      elsif counter == 51 || counter == 76
+        quarterString = quarterQuotient.floor.to_s + " quarters" + pennyMod.to_s + " penny"
+        outputString = quarterString
+      elsif counter >= 52 && counter <= 54 || counter >= 77 && counter <= 79
+        quarterString = quarterQuotient.floor.to_s + " quarters " + quarterMod.to_s + " pennies"
+        outputString = quarterString
+      elsif counter == 55 || counter == 80
+        quarterString = quarterQuotient.floor.to_s + " quarters " + nickelQuotient.to_s + " nickel"
+        outputString = quarterString
+      elsif counter == 56 || counter == 81
+        quarterString = quarterQuotient.floor.to_s + " quarters " + nickelQuotient.floor.to_s + " nickel " + pennyMod.to_s + " penny"
+        outputString = quarterString
+      elsif counter >= 57 && counter <= 59 || counter >= 82 && counter <= 84
+        quarterString = quarterQuotient.floor.to_s + " quarters " + nickelQuotient.floor.to_s  + " nickel " + pennyMod.to_s + " pennies"
+        outputString = quarterString
+      elsif counter == 60 || counter == 85
+      quarterString = quarterQuotient.floor.to_s + " quarters " + dimeQuotient.floor.to_s + " dime"
+        outputString = quarterString
+      elsif counter == 61 || counter == 86
+      quarterString = quarterQuotient.floor.to_s + " quarters " + dimeQuotient.floor.to_s + " dime " + pennyMod.to_s + " penny"
+        outputString = quarterString
+      elsif counter >= 62 && counter <= 64 || counter >= 87 && counter <= 89
+      quarterString = quarterQuotient.floor.to_s + " quarters " + dimeQuotient.floor.to_s + " dime " + pennyMod.to_s + " pennies"
+        outputString = quarterString
+      elsif counter == 65 || counter == 90
+      quarterString = quarterQuotient.floor.to_s + " quarters " + dimeQuotient.floor.to_s + " dime " + nickelQuotient.to_s + " nickel"
+        outputString = quarterString
+      elsif counter == 66 || counter == 91
+      quarterString = quarterQuotient.floor.to_s + " quarters " + dimeQuotient.floor.to_s + " dime " + nickelQuotient.to_s + " nickel " + pennyMod.to_s + " penny"
+        outputString = quarterString
+      elsif counter >= 67 && counter <= 69 || counter >= 92 && counter <= 94
+      quarterString = quarterQuotient.floor.to_s + " quarters " + dimeQuotient.floor.to_s + " dime " + nickelQuotient.to_s + " nickel " + pennyMod.to_s + " pennies"
+        outputString = quarterString
+      elsif counter == 70 || counter == 95
+      quarterString = quarterQuotient.floor.to_s + " quarters " + dimeQuotient.floor.to_s + " dimes"
+        outputString = quarterString
+      elsif counter == 71 || counter == 96
+      quarterString = quarterQuotient.floor.to_s + " quarters " + dimeQuotient.floor.to_s + " dimes " +  pennyMod.to_s + " penny"
+        outputString = quarterString
+      elsif counter >= 72 && counter <= 74 || counter >= 97 && counter <= 99
+      quarterString = quarterQuotient.floor.to_s + " quarters " + dimeQuotient.floor.to_s + " dimes " + pennyMod.to_s + " pennies"
+        outputString = quarterString
+      end
     end
-
     outputString
   end
 end
--- spec/coin_combination_spec.rb
+++ spec/coin_combination_spec.rb
@@ -12,32 +12,40 @@ describe('Fixnum#coin_combination') do
   it("test Fixnum for single nickel") do
     expect(5.coin_combination()).to(eq("1 nickel"))
   end
-  it("test Fixnum for modulo") do
+  it("test Fixnum for nickel and pennies") do
     expect(7.coin_combination()).to(eq("1 nickel 2 pennies"))
   end
   it("test Fixnum for single dime") do
     expect(10.coin_combination()).to(eq("1 dime"))
   end
-  it("test Fixnum for modulo") do
+  it("test Fixnum for dime and pennies") do
     expect(12.coin_combination()).to(eq("1 dime 2 pennies"))
   end
-  it("test Fixnum for modulo") do
+  it("test Fixnum for dime and nickel") do
     expect(15.coin_combination()).to(eq("1 dime 1 nickel"))
   end
-  it("test Fixnum for modulo") do
+  it("test Fixnum for dime nickel and penny") do
     expect(16.coin_combination()).to(eq("1 dime 1 nickel 1 penny"))
   end
-  it("test Fixnum for modulo") do
+  it("test Fixnum for dime nickel and pennies") do
     expect(17.coin_combination()).to(eq("1 dime 1 nickel 2 pennies"))
   end
-  it("test Fixnum for modulo") do
+  it("test Fixnum for dimes") do
     expect(20.coin_combination()).to(eq("2 dimes"))
   end
-  it("test Fixnum for modulo") do
+  it("test Fixnum for dimes and penny") do
     expect(21.coin_combination()).to(eq("2 dimes 1 penny"))
   end
-  it("test Fixnum for modulo") do
+  it("test Fixnum for dimes and pennies") do
     expect(23.coin_combination()).to(eq("2 dimes 3 pennies"))
   end
-
+  it("test Fixnum for quarters and pennies") do
+    expect(77.coin_combination()).to(eq("3 quarters 2 pennies"))
+  end
+  it("test Fixnum for quarters dimes and pennies") do
+    expect(99.coin_combination()).to(eq("3 quarters 2 dimes 4 pennies"))
+  end
+  it("test Fixnum for quarters dime nickel and pennies") do
+    expect(67.coin_combination()).to(eq("2 quarters 1 dime 1 nickel 2 pennies"))
+  end
 end
